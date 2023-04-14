0fb6667552ef9fde54d4aef4d4652f9d8afe4253
KristianK7 <119023461+KristianK7@users.noreply.github.com>
2023-04-14 04:18:42 -0400

    Add files via upload

--- Instance/Loop.py
+++ Instance/Loop.py
@@ -2,27 +2,53 @@ import numpy as np
 import os
 import subprocess
 import random
+import argparse as ap
+from datetime import datetime
+
+now = datetime.now()
+
+
+parser = ap.ArgumentParser()
+parser.add_argument('--n', type=str, default= '1', help='Numero de proceso')
+parser.add_argument('--i', type=int, default= 10, help='Numero de iteraciones')
+args = parser.parse_args()
 
 def main():
-    for j in range(4):
-        for i in range(25):
-            print("Iteracion: "+str(i+1)+", "+str(j+1))
-            python= "/bin/python3 "
-            RutaInstance= "/home/seminario/Instance/main.py "
-            azar = j+1
-            tipo = 'uniforme '
-            n = '100'
-            if azar == 2: n = '120'
-            if azar == 3: tipo = 'normal '
-            if azar == 4: 
-                n = '120'
-                tipo = 'normal '
+    archivo_log= "Instance/LOG_BSG.txt";
+    with open(archivo_log, 'a') as file:
+        file.write(
+            str(now)+', Inicio proceso '+args.n+"\n");
+        print("Inicio proceso "+args.n)
 
-            comand= python+RutaInstance+"--t "+tipo+"--p "+n
+    for j in range(4):
+        for i in range(args.i):
+            print(args.n)
+            python= "/bin/python3 "
+            RutaInstance= "Instance/main.py "
+            azar = j+1
+            tipo = 'Uniforme '
+            porcentaje = '100'
+            
+            if azar == 2: porcentaje = '120'
+            if azar == 3: tipo = 'Normal '
+            if azar == 4: 
+                porcentaje = '120'
+                tipo = 'Normal '
+            print("Iteracion: "+str(i+1)+", "+tipo)
+
+
+            comand= python+RutaInstance+"--t "+tipo+" --p "+porcentaje+" --i "+str(i+1)+" --n "+str(args.n)
             print(comand)
             out = subprocess.getoutput(comand)
             print (out)
             print ("\n")
+    
+    archivo_log= "Instance/LOG_BSG.txt";
+    with open(archivo_log, 'a') as file:
+        file.write(
+            str(now)+', Inicio proceso '+args.n+"\n");
+
+
 
 
 if __name__ == '__main__':
